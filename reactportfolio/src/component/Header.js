79ec6810a410c1ac7012b9905e0170344f970742
Shayna Murphy <110637697+Shaynam1977@users.noreply.github.com>
2023-03-03 16:53:34 -0800

    links

--- reactportfolio/src/component/Header.js
+++ reactportfolio/src/component/Header.js
@@ -1,9 +1,15 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 function Header() {
   return (
     <div>
-      <h2>This is the header</h2>
+      <h2>Shayna Murphy</h2>
+      <ul>
+        <li><Link to="/">About</Link></li>
+        <li><Link to="/portfolio">Portfolio</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
+        <li><Link to="/resume">Resume</Link></li>
+      </ul>
     </div>
   );
 }
